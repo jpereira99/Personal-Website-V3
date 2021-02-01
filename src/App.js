fb55952bc667a6793515f58076cab7a6b2fd7b8f
Jayden Pereira <jayden.pereira@yahoo.com>
2021-02-01 10:39:04 -0500

    Init CPE 322

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './pages/main';
+import CPE322 from './pages/cpe322';
 import CPE487 from './pages/cpe487';
 import CPE487Labs from './pages/cpe487.labs';
 import CPE487Project from './pages/cpe487.project';
@@ -11,6 +12,8 @@ function App() {
 		<Router>
 			<Switch>
 				<Route path='/' exact component={Main} />
+
+				<Route path='/projects/CPE322' exact component={CPE322} />
 
 				<Route path='/projects/CPE487' exact component={CPE487} />
 				<Route path='/projects/CPE487/labs' exact component={CPE487Labs} />
---
+++ src/pages/cpe322.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Collapsible from 'react-collapsible';
+import '../styles/cpe487.css';
+
+export default class CPE487 extends React.Component {
+	didOpenCollapsibleWithIndex(index) {
+		console.log('firing open');
+		var arrayOfCollapsibles = document.getElementsByClassName('Collapsible__contentOuter');
+		arrayOfCollapsibles[index].className = 'Collapsible__contentOuter is-open';
+	}
+	didCloseCollapsibleWithIndex(index) {
+		console.log('firing close');
+		var arrayOfCollapsibles = document.getElementsByClassName('Collapsible__contentOuter');
+		arrayOfCollapsibles[index].className = 'Collapsible__contentOuter is-closed';
+	}
+
+	render() {
+		return (
+			<div className='cpe487MainContainer'>
+				<h1 className='cpe487Title'>CPE 322 - Design VI</h1>
+				<a className='cpe487Subtitle' href='https://github.com/jpereira99/CPE-322'>
+					Click to View GitHub
+				</a>
+
+				{/* 
+				<div className='cpe487ButtonContainer'>
+					<Link to='/projects/cpe487/FlappyAttila' className='cpe487PageButton'>
+						<h2 className='cpe487PageButtonText'>Final Project</h2>
+					</Link>
+					<Link to='/projects/cpe487/labs' className='cpe487PageButton'>
+						<h2 className='cpe487PageButtonText'>Labs</h2>
+					</Link>
+				</div>
+				*/}
+				<div className='cpe487WeeklyReportContainer'>
+					{/* 
+					<Collapsible
+						trigger='[2020-10-23] Weekly Report #8'
+						onOpening={() => {
+							this.didOpenCollapsibleWithIndex(0);
+						}}
+						onClosing={() => {
+							this.didCloseCollapsibleWithIndex(0);
+						}}
+					>
+						<p>Completed Labs 1 and 2</p>
+					</Collapsible>
+
+					<Collapsible
+						trigger='[2020-10-16] Weekly Report #7'
+						onOpening={() => {
+							this.didOpenCollapsibleWithIndex(1);
+						}}
+						onClosing={() => {
+							this.didCloseCollapsibleWithIndex(1);
+						}}
+					>
+						<p>Finished reading textbook</p>
+					</Collapsible>
+
+					<Collapsible
+						trigger='[2020-10-09] Weekly Report #6'
+						onOpening={() => {
+							this.didOpenCollapsibleWithIndex(2);
+						}}
+						onClosing={() => {
+							this.didCloseCollapsibleWithIndex(2);
+						}}
+					>
+						<p>Read chapters 11 and 12</p>
+					</Collapsible>
+
+					<Collapsible
+						trigger='[2020-10-02] Weekly Report #5'
+						onOpening={() => {
+							this.didOpenCollapsibleWithIndex(3);
+						}}
+						onClosing={() => {
+							this.didCloseCollapsibleWithIndex(3);
+						}}
+					>
+						<p>Read chapters 9 and 10.</p>
+					</Collapsible>
+
+					<Collapsible
+						trigger='[2020-09-25] Weekly Report #4'
+						onOpening={() => {
+							this.didOpenCollapsibleWithIndex(4);
+						}}
+						onClosing={() => {
+							this.didCloseCollapsibleWithIndex(4);
+						}}
+					>
+						<p>
+							Read chapters 7 and 8 in the textbook. Joined Canvas group that had somoene that can get an FPGA so we can
+							actually get to work on it through the rest of the semester.
+						</p>
+					</Collapsible>
+
+					<Collapsible
+						trigger='[2020-09-18] Weekly Report #3'
+						onOpening={() => {
+							this.didOpenCollapsibleWithIndex(5);
+						}}
+						onClosing={() => {
+							this.didCloseCollapsibleWithIndex(5);
+						}}
+					>
+						<p>Read chapters 4, 5, and 6.</p>
+					</Collapsible>
+
+					<Collapsible
+						trigger='[2020-09-11] Weekly Report #2'
+						onOpening={() => {
+							this.didOpenCollapsibleWithIndex(6);
+						}}
+						onClosing={() => {
+							this.didCloseCollapsibleWithIndex(6);
+						}}
+					>
+						<p>Set up GHDL and GTKWave. Read Chapters 2 and 3.</p>
+					</Collapsible>
+*/}
+					<Collapsible
+						trigger='[2021-02-01] Weekly Report #1'
+						onOpening={() => {
+							this.didOpenCollapsibleWithIndex(0);
+						}}
+						onClosing={() => {
+							this.didCloseCollapsibleWithIndex(0);
+						}}
+					>
+						<p>*In Progress*</p>
+					</Collapsible>
+				</div>
+
+				<a href='/#projects' className='cpe487BackButton'>
+					Back to Projects
+				</a>
+			</div>
+		);
+	}
+}
